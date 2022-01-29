28fd268ca0a0af0dfdd97e6d8db1fde52c4473f6
Methodician <methodician@gmail.com>
2022-01-29 15:06:26 -0800

    Sending periodic temp readings with timestamp to firebase

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@
 
 #include "config.h"
 
+// TODO: Should probably be in its own file
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
+
+#define FIREBASE_NODE "/readouts/" + FURBALL_ID
 
 // Define Firebase Data object
 FirebaseData fbdo;
@@ -19,8 +24,14 @@ unsigned long sendDataPrevMillis = 0;
 
 unsigned long count = 0;
 
+// TODO: Should probably be in its own file similar to Furball
+Adafruit_BME680 bme;
+
+
+
 void setup() {
 
+// Set up Firebase, WiFi, etc...
   Serial.begin(115200);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
@@ -76,6 +87,18 @@ void setup() {
   You can also set the TCP data sending retry with
   config.tcp_data_sending_retry = 1;
   */
+
+ // Set up bme680
+ //! some boards may use bme280 instead
+ // TODO: put this in its own file, in a void begin()
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    return;
+  }
+
+  bme.setTemperatureOversampling(BME680_OS_8X);
+
+
 }
 
 // the loop function runs over and over again forever
@@ -83,68 +106,26 @@ void loop(){
 
   //Firebase.ready works for authentication management and should be called repeatedly in the loop.
 
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0))
   {
     sendDataPrevMillis = millis();
 
-    Serial.printf("Set bool... %s\n", Firebase.RTDB.setBool(&fbdo, F("/test/bool"), count % 2 == 0) ? "ok" : fbdo.errorReason().c_str());
+  //! BME stuff should probably go in bme files as handlers like in Furball
+  FirebaseJson bmeReadings;
+  float temp = bme.readTemperature();
 
-    Serial.printf("Get bool... %s\n", Firebase.RTDB.getBool(&fbdo, FPSTR("/test/bool")) ? fbdo.to<bool>() ? "true" : "false" : fbdo.errorReason().c_str());
+  bmeReadings.set("bme/temperature", temp);
+  bmeReadings.set("timestamp/.sv", "timestamp");
 
-    bool bVal;
-    Serial.printf("Get bool ref... %s\n", Firebase.RTDB.getBool(&fbdo, F("/test/bool"), &bVal) ? bVal ? "true" : "false" : fbdo.errorReason().c_str());
-
-    Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, F("/test/int"), count) ? "ok" : fbdo.errorReason().c_str());
-
-    Serial.printf("Get int... %s\n", Firebase.RTDB.getInt(&fbdo, F("/test/int")) ? String(fbdo.to<int>()).c_str() : fbdo.errorReason().c_str());
-
-    int iVal = 0;
-    Serial.printf("Get int ref... %s\n", Firebase.RTDB.getInt(&fbdo, F("/test/int"), &iVal) ? String(iVal).c_str() : fbdo.errorReason().c_str());
-
-    Serial.printf("Set float... %s\n", Firebase.RTDB.setFloat(&fbdo, F("/test/float"), count + 10.2) ? "ok" : fbdo.errorReason().c_str());
-
-    Serial.printf("Get float... %s\n", Firebase.RTDB.getFloat(&fbdo, F("/test/float")) ? String(fbdo.to<float>()).c_str() : fbdo.errorReason().c_str());
-
-    Serial.printf("Set double... %s\n", Firebase.RTDB.setDouble(&fbdo, F("/test/double"), count + 35.517549723765) ? "ok" : fbdo.errorReason().c_str());
-
-    Serial.printf("Get double... %s\n", Firebase.RTDB.getDouble(&fbdo, F("/test/double")) ? String(fbdo.to<double>()).c_str() : fbdo.errorReason().c_str());
-
-    Serial.printf("Set string... %s\n", Firebase.RTDB.setString(&fbdo, F("/test/string"), F("Hello World!")) ? "ok" : fbdo.errorReason().c_str());
-
-    Serial.printf("Get string... %s\n", Firebase.RTDB.getString(&fbdo, F("/test/string")) ? fbdo.to<const char *>() : fbdo.errorReason().c_str());
-
-    //For the usage of FirebaseJson, see examples/FirebaseJson/BasicUsage/Create_Edit_Parse.ino
-    FirebaseJson json;
-
-    if (count == 0)
-    {
-      json.set("value/round/" + String(count), F("cool!"));
-      json.set(F("value/ts/.sv"), F("timestamp"));
-      Serial.printf("Set json... %s\n", Firebase.RTDB.set(&fbdo, F("/test/json"), &json) ? "ok" : fbdo.errorReason().c_str());
-    }
-    else
-    {
-      json.add(String(count), F("smart!"));
-      Serial.printf("Update node... %s\n", Firebase.RTDB.updateNode(&fbdo, F("/test/json/value/round"), &json) ? "ok" : fbdo.errorReason().c_str());
-    }
-
-    Serial.println();
-
-    //For generic set/get functions.
-
-    //For generic set, use Firebase.RTDB.set(&fbdo, <path>, <any variable or value>)
-
-    //For generic get, use Firebase.RTDB.get(&fbdo, <path>).
-    //And check its type with fbdo.dataType() or fbdo.dataTypeEnum() and
-    //cast the value from it e.g. fbdo.to<int>(), fbdo.to<std::string>().
-
-    //The function, fbdo.dataType() returns types String e.g. string, boolean,
-    //int, float, double, json, array, blob, file and null.
-
-    //The function, fbdo.dataTypeEnum() returns type enum (number) e.g. fb_esp_rtdb_data_type_null (1),
-    //fb_esp_rtdb_data_type_integer, fb_esp_rtdb_data_type_float, fb_esp_rtdb_data_type_double,
-    //fb_esp_rtdb_data_type_boolean, fb_esp_rtdb_data_type_string, fb_esp_rtdb_data_type_json,
-    //fb_esp_rtdb_data_type_array, fb_esp_rtdb_data_type_blob, and fb_esp_rtdb_data_type_file (10)
+  Serial.printf("Push data with timestamp... %s\n", Firebase.RTDB.pushJSON(&fbdo, READOUTS_NODE, &bmeReadings) ? "ok" : fbdo.errorReason().c_str());
+  // bmeReadings.set("timestamp/.sv", "timestamp");
+  // bmeReadings.add("temperature/" + String(count), temp);
+  // Firebase.RTDB.updateNode(&fbdo, F("/bme"), &bmeReadings);
+  // Serial.print(F("Temperature = "));
+  // Serial.print(temp);
+  // Serial.print(F(" *C and "));
+  // Serial.print(temp * 9/5 + 32);
+  // Serial.println(F(" *F"));
 
     count++;
   }
